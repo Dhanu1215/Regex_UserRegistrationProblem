54f314936613c436f84c621fe97aab391df40b03
Asus <dhanashreehakke7@gmail.com>
2021-11-12 14:26:42 +0530

    Added: Last name check file is added

--- src/main/java/com/blp/regex/UserRegistration.java
+++ src/main/java/com/blp/regex/UserRegistration.java
@@ -4,8 +4,13 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static boolean firstName(String name){
-        boolean check = Pattern.matches("([A-Z][a-z]{3,})",name);
+    public static boolean firstName(String name) {
+        boolean check = Pattern.matches("([A-Z][a-z]{3,})", name);
+        return check;
+    }
+
+    public static boolean lastName(String name) {
+        boolean check = Pattern.matches("([A-Z][a-z]{3,})", name);
         return check;
     }
 }
--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 public class TestUserRegistration {
 
-     @Test
-     public void checkValidName(){
-      boolean check = UserRegistration.firstName("Darshana");
-          Assert.assertSame(true,check);
-     }
+    @Test
+    public void checkValidName() {
+        boolean check = UserRegistration.firstName("Darshana");
+        Assert.assertSame(true, check);
+    }
+
+    @Test
+    public void checkValidName1() {
+        boolean check = UserRegistration.lastName("Hakke");
+        Assert.assertSame(true, check);
+    }
 }
