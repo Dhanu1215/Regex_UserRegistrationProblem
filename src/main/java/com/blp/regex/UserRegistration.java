bcaaaf29b78e71e3b03e42b9436b36bc51626d1d
Asus <dhanashreehakke7@gmail.com>
2021-11-12 13:27:35 +0530

    Added: Welcome message is added

---
+++ src/main/java/com/blp/regex/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.blp.regex;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to user registration problems");
+    }
+}
