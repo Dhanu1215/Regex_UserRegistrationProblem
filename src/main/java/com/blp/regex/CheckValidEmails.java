37dcff66e4dc37ed3d71b9e45aa96ee429e82e2a
Asus <dhanashreehakke7@gmail.com>
2021-11-15 15:50:14 +0530

    Added: Emails validation file is added

--- src/main/java/com/blp/regex/CheckValidEmails.java
+++ src/main/java/com/blp/regex/CheckValidEmails.java
@@ -6,50 +6,22 @@ import java.util.regex.Pattern;
 
 public class CheckValidEmails {
 
-    public static void main(String[] args) {
-
-    }
-
-    public static boolean checkEmails() {
-        List<String> emails = new ArrayList<String>();
-        emails.add("abc@yahoo.com");
-        emails.add("abc-100@yahoo.com");
-        emails.add("abc.100@yahoo.com");
-        emails.add("abc111@abc.com");
-        emails.add("abc-100@abc.net");
-        emails.add("abc.100@abc.com.au");
-        emails.add("abc@1.com");
-        emails.add("abc@gmail.com.com");
-        emails.add("abc+100@gmail.com");
-        Boolean temp = false;
-        for (String email : emails) {
-            boolean check = Pattern.matches("^([a-z0-9]{3}.+)@[a-z0-9]+(.+)$", email);
-            temp = check;
+    public static boolean checkEmails(String email) {
+        boolean check = Pattern.matches("(([a-z A-Z]{3,})([-$&+,:;=?#|'<>.^*()%!]?[0-9 a-z A-Z]{3,})?+@([0-9 a-z]{1,})([.])([a-z]{2,})([.])?([a-z]{2,})?)", email);
+        if (check != true) {
+            System.out.println("Enter valid email");
         }
+        Boolean temp = check;
         return temp;
     }
 
-    public static boolean checkInvalidEmails() {
-        List<String> emails = new ArrayList<String>();
-        emails.add("abc");
-        emails.add("abc@.com.my");
-        emails.add("abc123@gmail.a");
-        emails.add("abc123@.com");
-        emails.add("abc123@.com.com");
-        emails.add(".abc@abc.com");
-        emails.add("abc()*@gmail.com");
-        emails.add("abc@%*.com");
-        emails.add("abc..2002@gmail.com");
-        emails.add("abc.@gmail.com");
-        emails.add("abc@abc@gmail.com");
-        emails.add("abc@gmail.com.1a");
-        emails.add("abc@gmail.com.aa.au");
 
-        Boolean temp = false;
-        for (String email : emails) {
-            boolean check = Pattern.matches("^([a-z0-9]{3}.+)@[a-z]{2,}(.+)$", email);
-            temp = check;
+    public static boolean checkInvalidEmails(String email) {
+        boolean check = Pattern.matches("(([a-z A-Z]{3,})([-$&+,:;=?#|'<>.^*()%!]?[0-9 a-z A-Z]{3,})?+@([0-9 a-z]{1,})([.])([a-z]{2,})([.])?([a-z]{2,})?)", email);
+        if (check == true) {
+            System.out.println("Entered email is valid");
         }
+        Boolean temp = check;
         return temp;
     }
 }
